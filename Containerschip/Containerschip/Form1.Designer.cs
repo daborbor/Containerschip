b6679edfee5b42cc7d8fe0b1a01f934e0b3dfcc6
Boris <github@borispeeters.nl>
2019-06-11 15:35:39 +0200

    GUI beter gemaakt

--- Containerschip/Containerschip/Form1.Designer.cs
+++ Containerschip/Containerschip/Form1.Designer.cs
@@ -28,28 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lbContainer = new System.Windows.Forms.ListBox();
             this.btnAddContainer = new System.Windows.Forms.Button();
             this.nudContainerWeight = new System.Windows.Forms.NumericUpDown();
             this.nudX = new System.Windows.Forms.NumericUpDown();
             this.nudY = new System.Windows.Forms.NumericUpDown();
             this.butMakeShip = new System.Windows.Forms.Button();
+            this.lbContainer = new System.Windows.Forms.ListBox();
+            this.btnSorteer = new System.Windows.Forms.Button();
+            this.cbWaardevol = new System.Windows.Forms.CheckBox();
+            this.cbElektriciteit = new System.Windows.Forms.CheckBox();
+            this.nudRandom = new System.Windows.Forms.NumericUpDown();
+            this.btnRandom = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.nudContainerWeight)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.nudX)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.nudY)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudRandom)).BeginInit();
             this.SuspendLayout();
-            // 
-            // lbContainer
-            // 
-            this.lbContainer.FormattingEnabled = true;
-            this.lbContainer.Location = new System.Drawing.Point(12, 12);
-            this.lbContainer.Name = "lbContainer";
-            this.lbContainer.Size = new System.Drawing.Size(120, 95);
-            this.lbContainer.TabIndex = 0;
             // 
             // btnAddContainer
             // 
-            this.btnAddContainer.Location = new System.Drawing.Point(138, 418);
+            this.btnAddContainer.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnAddContainer.Location = new System.Drawing.Point(138, 420);
             this.btnAddContainer.Name = "btnAddContainer";
             this.btnAddContainer.Size = new System.Drawing.Size(107, 23);
             this.btnAddContainer.TabIndex = 1;
@@ -59,9 +58,15 @@
             // 
             // nudContainerWeight
             // 
-            this.nudContainerWeight.Location = new System.Drawing.Point(12, 421);
+            this.nudContainerWeight.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.nudContainerWeight.Location = new System.Drawing.Point(12, 423);
+            this.nudContainerWeight.Maximum = new decimal(new int[] {
+            30000,
+            0,
+            0,
+            0});
             this.nudContainerWeight.Minimum = new decimal(new int[] {
-            1,
+            4000,
             0,
             0,
             0});
@@ -69,28 +74,51 @@
             this.nudContainerWeight.Size = new System.Drawing.Size(120, 20);
             this.nudContainerWeight.TabIndex = 2;
             this.nudContainerWeight.Value = new decimal(new int[] {
-            1,
+            4000,
             0,
             0,
             0});
             // 
             // nudX
             // 
-            this.nudX.Location = new System.Drawing.Point(251, 421);
+            this.nudX.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.nudX.Location = new System.Drawing.Point(251, 423);
+            this.nudX.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
             this.nudX.Name = "nudX";
             this.nudX.Size = new System.Drawing.Size(120, 20);
             this.nudX.TabIndex = 3;
+            this.nudX.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
             // 
             // nudY
             // 
-            this.nudY.Location = new System.Drawing.Point(377, 421);
+            this.nudY.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.nudY.Location = new System.Drawing.Point(377, 423);
+            this.nudY.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
             this.nudY.Name = "nudY";
             this.nudY.Size = new System.Drawing.Size(120, 20);
             this.nudY.TabIndex = 4;
+            this.nudY.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
             // 
             // butMakeShip
             // 
-            this.butMakeShip.Location = new System.Drawing.Point(503, 418);
+            this.butMakeShip.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.butMakeShip.Location = new System.Drawing.Point(503, 420);
             this.butMakeShip.Name = "butMakeShip";
             this.butMakeShip.Size = new System.Drawing.Size(81, 23);
             this.butMakeShip.TabIndex = 5;
@@ -98,11 +126,84 @@
             this.butMakeShip.UseVisualStyleBackColor = true;
             this.butMakeShip.Click += new System.EventHandler(this.butMakeShip_Click);
             // 
+            // lbContainer
+            // 
+            this.lbContainer.FormattingEnabled = true;
+            this.lbContainer.Location = new System.Drawing.Point(12, 12);
+            this.lbContainer.Name = "lbContainer";
+            this.lbContainer.Size = new System.Drawing.Size(120, 264);
+            this.lbContainer.TabIndex = 0;
+            // 
+            // btnSorteer
+            // 
+            this.btnSorteer.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnSorteer.AutoSize = true;
+            this.btnSorteer.Location = new System.Drawing.Point(590, 420);
+            this.btnSorteer.Name = "btnSorteer";
+            this.btnSorteer.Size = new System.Drawing.Size(81, 23);
+            this.btnSorteer.TabIndex = 6;
+            this.btnSorteer.Text = "Sorteer";
+            this.btnSorteer.UseVisualStyleBackColor = true;
+            // 
+            // cbWaardevol
+            // 
+            this.cbWaardevol.AutoSize = true;
+            this.cbWaardevol.Location = new System.Drawing.Point(12, 400);
+            this.cbWaardevol.Name = "cbWaardevol";
+            this.cbWaardevol.Size = new System.Drawing.Size(78, 17);
+            this.cbWaardevol.TabIndex = 7;
+            this.cbWaardevol.Text = "Waardevol";
+            this.cbWaardevol.UseVisualStyleBackColor = true;
+            // 
+            // cbElektriciteit
+            // 
+            this.cbElektriciteit.AutoSize = true;
+            this.cbElektriciteit.Location = new System.Drawing.Point(12, 377);
+            this.cbElektriciteit.Name = "cbElektriciteit";
+            this.cbElektriciteit.Size = new System.Drawing.Size(77, 17);
+            this.cbElektriciteit.TabIndex = 8;
+            this.cbElektriciteit.Text = "Elektriciteit";
+            this.cbElektriciteit.UseVisualStyleBackColor = true;
+            // 
+            // nudRandom
+            // 
+            this.nudRandom.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.nudRandom.Location = new System.Drawing.Point(12, 282);
+            this.nudRandom.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudRandom.Name = "nudRandom";
+            this.nudRandom.Size = new System.Drawing.Size(120, 20);
+            this.nudRandom.TabIndex = 9;
+            this.nudRandom.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // btnRandom
+            // 
+            this.btnRandom.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnRandom.Location = new System.Drawing.Point(12, 308);
+            this.btnRandom.Name = "btnRandom";
+            this.btnRandom.Size = new System.Drawing.Size(134, 23);
+            this.btnRandom.TabIndex = 10;
+            this.btnRandom.Text = "Voeg random containers toe";
+            this.btnRandom.UseVisualStyleBackColor = true;
+            this.btnRandom.Click += new System.EventHandler(this.btnRandom_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(818, 452);
+            this.Controls.Add(this.btnRandom);
+            this.Controls.Add(this.nudRandom);
+            this.Controls.Add(this.cbElektriciteit);
+            this.Controls.Add(this.cbWaardevol);
+            this.Controls.Add(this.btnSorteer);
             this.Controls.Add(this.butMakeShip);
             this.Controls.Add(this.nudY);
             this.Controls.Add(this.nudX);
@@ -114,18 +215,24 @@
             ((System.ComponentModel.ISupportInitialize)(this.nudContainerWeight)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.nudX)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.nudY)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudRandom)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.ListBox lbContainer;
         private System.Windows.Forms.Button btnAddContainer;
         private System.Windows.Forms.NumericUpDown nudContainerWeight;
         private System.Windows.Forms.NumericUpDown nudX;
         private System.Windows.Forms.NumericUpDown nudY;
         private System.Windows.Forms.Button butMakeShip;
+        private System.Windows.Forms.ListBox lbContainer;
+        private System.Windows.Forms.Button btnSorteer;
+        private System.Windows.Forms.CheckBox cbWaardevol;
+        private System.Windows.Forms.CheckBox cbElektriciteit;
+        private System.Windows.Forms.NumericUpDown nudRandom;
+        private System.Windows.Forms.Button btnRandom;
     }
 }
 
--- Containerschip/Containerschip/Form1.cs
+++ Containerschip/Containerschip/Form1.cs
@@ -1,11 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 using Algoritme.Objects;
 using System.Windows.Forms;
+using Algoritme.Interfaces.ObjectInterfaces;
 
 namespace Containerschip
 {
     public partial class Form1 : Form
     {
+        private Random r = new Random();
+        private Ship ship;
+        private List<Button> buttonList = new List<Button>();
+
         public Form1()
         {
             InitializeComponent();
@@ -26,9 +32,42 @@ namespace Containerschip
 
         private void butMakeShip_Click(object sender, EventArgs e)
         {
-            for (int i = 0; nudY.Value != i; i++)
+            foreach (Button button in buttonList)
             {
+                Controls.Remove(button);
+            }
+            buttonList.Clear();
+            Ship ship = new Ship((int) nudX.Value, (int) nudY.Value);
+            List<List<IStack>> shipList = ship.GetList();
+            foreach (List<IStack> stackList in shipList)
+            {
+                foreach (IStack stack in stackList)
+                {
+                    int x = shipList.IndexOf(stackList);
+                    int y = stackList.IndexOf(stack);
+                    Button newButton = new Button();
+                    newButton.Text = $"{x}, {y}";
+                    newButton.Left = 150 + x * 80;
+                    newButton.Top = y * 30;
+                    newButton.Name = $"{x},{y}";
+                    buttonList.Add(newButton);
+                }
+            }
 
+            foreach (Button Button in buttonList)
+            {
+                Controls.Add(Button);
+            }
+        }
+
+        private void btnRandom_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < nudRandom.Value; i++)
+            {
+                int weight = r.Next(4000, 30000);
+                bool electricity = 0.5 < r.NextDouble();
+                bool valuable = 0.5 < r.NextDouble();
+                lbContainer.Items.Add(new StandardContainer(weight, electricity, valuable));
             }
         }
     }

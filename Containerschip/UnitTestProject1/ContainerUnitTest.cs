16d7d62adf07777b3deb823c6ba56ed13f3b6e90
Boris <github@borispeeters.nl>
2019-06-18 15:48:11 +0200

    Meer unittests gemaakt

---
+++ Containerschip/UnitTestProject1/ContainerUnitTest.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Objects.Interfaces.ObjectInterfaces;
+using Objects.Objects.Container;
+using System;
+
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class ContainerUnitTest
+    {
+        [TestMethod]
+        public void KloptHetGewicht()
+        {
+            IContainer container = new Container(100, false, false);
+            Assert.AreEqual(100, container.Weight);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void MagGewichtKleinDan1Zijn()
+        {
+            IContainer container = new Container(0, false, false);
+        }
+
+        [TestMethod]
+        public void CheckElectricityProp()
+        {
+            IContainer containerE = new Container(1, true, false);
+            IContainer containerNonE = new Container(1, false, false);
+            Assert.IsTrue(containerE.Electricity);
+            Assert.IsFalse(containerNonE.Electricity);
+        }
+
+        [TestMethod]
+        public void CheckValuableProp()
+        {
+            IContainer containerV = new Container(1, false, true);
+            IContainer containerNonV = new Container(1, false, false);
+            Assert.IsTrue(containerV.Valuable);
+            Assert.IsFalse(containerNonV.Valuable);
+        }
+    }
+}
--- Containerschip/UnitTestProject1/ShipUnitTest.cs
+++ Containerschip/UnitTestProject1/ShipUnitTest.cs
@@ -1,31 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Algoritme.Objects;
-using Algoritme.Objects.Ship;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Objects.Objects.Container;
+using Objects.Objects.Ship;
 
 namespace UnitTestProject1
 {
     [TestClass]
-    class ShipUnitTest
+    public class ShipUnitTest
     {
         [TestMethod]
         public void KanErEenSchipGemaaktWorden()
         {
-            Ship ship = new Ship(4,4);
+            Ship ship = new Ship(4, 5);
+
+            int x = ship.GetList().Count;
+            int y = ship.GetList()[0].Count;
+
+            Assert.AreEqual(4, ship.WidthX);
+            Assert.AreEqual(5, ship.LengthY);
+            Assert.AreEqual(4, x);
+            Assert.AreEqual(5, y);
         }
 
         [TestMethod]
         public void KloptHetGewicht()
         {
+            Ship ship = new Ship(4, 5);
 
-        }
+            ship.AddContainer(0,1, new Container(5500, false, false));
+            ship.AddContainer(3,1, new Container(4500, false, false));
 
-        [TestMethod]
-        public void KloptHetEvenwicht()
-        {
-
+            Assert.AreEqual(5500, ship.GewichtLinks());
+            Assert.AreEqual(4500, ship.GewichtRechts());
         }
     }
 }

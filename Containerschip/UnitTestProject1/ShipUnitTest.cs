e54e40d094924a6e1f86c61d992e53c75464c21e
Boris <github@borispeeters.nl>
2019-06-19 22:55:46 +0200

    nieuw unittests (coverage meer dan 89%)

--- Containerschip/UnitTestProject1/ShipUnitTest.cs
+++ Containerschip/UnitTestProject1/ShipUnitTest.cs
@@ -26,6 +26,18 @@ namespace UnitTestProject1
         [TestMethod]
         public void KloptHetGewicht()
         {
+            List<int> powerStackRows = new List<int> { 0 };
+            Ship ship = new Ship(4, 5, 2000000, 120000, powerStackRows);
+
+            ship.AddContainer(0, 1, new Container(5500, false, false));
+            ship.AddContainer(3, 1, new Container(4500, false, false));
+
+            Assert.AreEqual(10000, ship.Weight);
+        }
+
+        [TestMethod]
+        public void KloptHetGewichtLinksRechts()
+        {
             List<int> powerStackRows = new List<int> {0};
             Ship ship = new Ship(4, 5, 2000000, 120000, powerStackRows);
 
@@ -34,6 +46,14 @@ namespace UnitTestProject1
 
             Assert.AreEqual(5500, ship.GewichtLinks());
             Assert.AreEqual(4500, ship.GewichtRechts());
+        }
+        [TestMethod]
+        public void KloptHetMaxGewicht()
+        {
+            List<int> powerStackRows = new List<int> { 0 };
+            Ship ship = new Ship(2, 2, 2000000, 120000, powerStackRows);
+
+            Assert.AreEqual((4* 2000000) ,ship.MaxWeight);
         }
     }
 }
